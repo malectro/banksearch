679e5d5c92e8a65bebddde1a120ba648e6bbedfb
Kyle Warren <qarren@gmail.com>
2013-01-19 13:59:53 -0500

    added all the ruby files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banksearch::Application.config.secret_token = 'af5564dafe1c0921dd14afa13e308dfd1e582be8a4f2acc2b0124f9a2401ba25df501ac10404aa1c520a54c2f4414a4d2fc0184e10043fff7f625c14ee27787c'
