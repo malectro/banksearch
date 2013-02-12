efc0fc1c73251f435ecadecd6b9c55711976db81
Kyle Warren <qarren@gmail.com>
2013-02-12 15:47:43 -0500

    added precompilation mainifest calls for admin

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Banksearch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['admin/admin.js', 'admin/admin.css.scss']
   end
 end
